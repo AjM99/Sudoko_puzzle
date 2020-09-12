b6afe631428f639f0ba055c1fed778fbf4f1e916
Anuj MOre <anuj.more10424@gmail.com>
2020-09-12 11:07:23 +0530

    1s

---
+++ first.py
@@ -0,0 +1,26 @@
+print('hello world')
+
+# for talking input form user
+a = input('enter your name : ')
+print('welcome', a) 
+
+
+# conditional statement (if else)
+b= 5
+if b > 10 :
+    print('a')   
+    print('aa')
+    if b > 5 :
+        print('lol')
+else :        
+    print('bbbb')    
+
+#else if of python 'elif'
+
+x = 3
+if x  < 2 :
+    print('hi')
+elif x < 4 :
+    print('hello')
+else :
+    print('lol')    
