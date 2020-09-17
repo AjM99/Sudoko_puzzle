ec4ac6c21169683c19bb5eb4d4973114932152f5
Anuj MOre <anuj.more10424@gmail.com>
2020-09-17 17:06:32 +0530

    5p

---
+++ functions.py
@@ -0,0 +1,10 @@
+# they are basically store and reuse 
+
+def LOL() :                                                    #defining a function 
+    print('this is function output')
+    print('yes it is')
+
+a = input('enter name : ')
+print('welcome', a)
+LOL()                                                       #calling a function     
+print('all good')
